bee667f0d61afa4b3071b47b6fc4702d4ed0e20f
soumik12345 <19soumik.rakshit96@gmail.com>
2024-05-26 19:38:11 +0000

    update: scripts

--- scripts/evaluate_weave.py
+++ scripts/evaluate_weave.py
@@ -19,10 +19,6 @@ if __name__ == "__main__":
     clip_iqa_scorer = CLIPImageQualityScorer()
     diffuion_evaluation_pipeline.add_metric(clip_iqa_scorer)
 
-    # Add BLIP Scorer Metric
-    blip_scorer = BLIPScorer()
-    diffuion_evaluation_pipeline.add_metric(blip_scorer)
-
     diffuion_evaluation_pipeline(
         dataset="parti-prompts:v1",
         init_params=dict(project="t2i_eval", entity="geekyrakshit"),
