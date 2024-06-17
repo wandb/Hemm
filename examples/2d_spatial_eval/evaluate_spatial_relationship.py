23d8add0c397b1483d047571c693b151aa7e43d3
soumik12345 <19soumik.rakshit96@gmail.com>
2024-06-17 18:54:17 +0000

    update: examples

--- examples/2d_spatial_eval/evaluate_spatial_relationship.py
+++ examples/2d_spatial_eval/evaluate_spatial_relationship.py
@@ -1,38 +1,59 @@
 import os
+from typing import Optional, Tuple
 
 import fire
 import jsonlines
-import rich
 import wandb
 import weave
 
-from hemm.eval_pipelines import StableDiffusionEvaluationPipeline
+from hemm.eval_pipelines import BaseWeaveModel, EvaluationPipeline
 from hemm.metrics.spatial_relationship import SpatialRelationshipMetric2D
 from hemm.metrics.spatial_relationship.judges import DETRSpatialRelationShipJudge
 
 
-def main():
-    spatial_prompt_file = os.path.join(
-        "/home/ubuntu/Hemm/artifacts/t2i_compbench_spatial_prompts:v1", "spatial.jsonl"
-    )
-    with jsonlines.open(spatial_prompt_file) as reader:
-        for obj in reader:
-            dataset = obj[:5]
+def main(
+    project="2d-spatial-relationship",
+    entity="hemm-eval",
+    dataset_artifact: str = "hemm-eval/2d-spatial-relationship/t2i_compbench_spatial_prompts:v0",
+    dataset_ref: Optional[str] = "t2i_compbench_spatial_prompts:v0",
+    dataset_limit: Optional[int] = None,
+    diffusion_model_address: str = "stabilityai/stable-diffusion-2-1",
+    diffusion_model_enable_cpu_offfload: bool = False,
+    image_size: Tuple[int, int] = (512, 512),
+    detr_model_address: str = "facebook/detr-resnet-50",
+    detr_revision: str = "no_timm",
+):
+    wandb.init(project=project, entity=entity, job_type="evaluation")
+    weave.init(project_name=project)
+    artifact = wandb.use_artifact(dataset_artifact, type="dataset")
+    artifact_dir = artifact.download()
 
-    diffuion_evaluation_pipeline = StableDiffusionEvaluationPipeline(
-        "stabilityai/stable-diffusion-2-1"
-    )
+    if dataset_limit:
+        spatial_prompt_file = os.path.join(artifact_dir, "spatial.jsonl")
+        with jsonlines.open(spatial_prompt_file) as reader:
+            for obj in reader:
+                dataset = obj[:dataset_limit]
+    else:
+        dataset = weave.ref(dataset_ref).get()
 
-    judge = DETRSpatialRelationShipJudge()
-    metric = SpatialRelationshipMetric2D(judge=judge)
-    diffuion_evaluation_pipeline.add_metric(metric)
-
-    diffuion_evaluation_pipeline(
-        dataset=dataset,
-        init_params=dict(project="2d-spatial-relationship", entity="hemm-eval"),
-        job_name="test",
+    diffusion_model = BaseWeaveModel(
+        diffusion_model_name_or_path=diffusion_model_address,
+        enable_cpu_offfload=diffusion_model_enable_cpu_offfload,
+        image_height=image_size[0],
+        image_width=image_size[1],
     )
+    evaluation_pipeline = EvaluationPipeline(model=diffusion_model)
+
+    judge = DETRSpatialRelationShipJudge(
+        model_address=detr_model_address, revision=detr_revision
+    )
+    metric = SpatialRelationshipMetric2D(
+        judge=judge, name="2d_spatial_relationship_score"
+    )
+    evaluation_pipeline.add_metric(metric)
+
+    evaluation_pipeline(dataset=dataset)
 
 
 if __name__ == "__main__":
-    main()
+    fire.Fire(main)
--- examples/evaluate_weave_prompt_alignment.py
+++ examples/evaluate_weave_prompt_alignment.py
@@ -1,6 +1,8 @@
 import fire
+import wandb
+import weave
 
-from hemm.eval_pipelines import StableDiffusionEvaluationPipeline
+from hemm.eval_pipelines import BaseWeaveModel, EvaluationPipeline
 from hemm.metrics.prompt_alignment import CLIPScoreMetric, CLIPImageQualityScoreMetric
 
 
@@ -8,28 +10,30 @@ def main(
     diffusion_model_name_or_path: str = "CompVis/stable-diffusion-v1-4",
     clip_model_name_or_path: str = "openai/clip-vit-base-patch16",
     clip_iqa_model_name_or_path: str = "clip_iqa",
+    diffusion_model_enable_cpu_offfload: bool = False,
     dataset: str = "parti-prompts:v0",
     project: str = "propmpt-alignment",
 ):
-    diffuion_evaluation_pipeline = StableDiffusionEvaluationPipeline(
-        diffusion_model_name_or_path
+    wandb.init(project=project, job_type="evaluation")
+    weave.init(project_name=project)
+
+    model = BaseWeaveModel(
+        diffusion_model_name_or_path=diffusion_model_name_or_path,
+        enable_cpu_offfload=diffusion_model_enable_cpu_offfload,
     )
+    evaluation_pipeline = EvaluationPipeline(model=model)
 
     # Add CLIP Scorer metric
     clip_scorer = CLIPScoreMetric(clip_model_name_or_path=clip_model_name_or_path)
-    diffuion_evaluation_pipeline.add_metric(clip_scorer)
+    evaluation_pipeline.add_metric(clip_scorer)
 
     # Add CLIP IQA Metric
     clip_iqa_scorer = CLIPImageQualityScoreMetric(
         clip_model_name_or_path=clip_iqa_model_name_or_path
     )
-    diffuion_evaluation_pipeline.add_metric(clip_iqa_scorer)
+    evaluation_pipeline.add_metric(clip_iqa_scorer)
 
-    diffuion_evaluation_pipeline(
-        dataset=dataset,
-        init_params=dict(project=project),
-        job_name="test",
-    )
+    evaluation_pipeline(dataset=dataset)
 
 
 if __name__ == "__main__":
