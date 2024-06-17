50734737a50727ee61ee3e1bed4f866faa042963
soumik12345 <19soumik.rakshit96@gmail.com>
2024-06-17 13:18:01 +0000

    update: examples

--- examples/evaluate_weave_prompt_alignment.py
+++ examples/evaluate_weave_prompt_alignment.py
@@ -1,21 +1,36 @@
+import fire
+
 from hemm.eval_pipelines import StableDiffusionEvaluationPipeline
 from hemm.metrics.prompt_alignment import CLIPScoreMetric, CLIPImageQualityScoreMetric
 
 
-if __name__ == "__main__":
+def main(
+    diffusion_model_name_or_path: str = "CompVis/stable-diffusion-v1-4",
+    clip_model_name_or_path: str = "openai/clip-vit-base-patch16",
+    clip_iqa_model_name_or_path: str = "clip_iqa",
+    dataset: str = "parti-prompts:v0",
+    project: str = "propmpt-alignment",
+):
     diffuion_evaluation_pipeline = StableDiffusionEvaluationPipeline(
-        "CompVis/stable-diffusion-v1-4"
+        diffusion_model_name_or_path
     )
 
     # Add CLIP Scorer metric
-    clip_scorer = CLIPScoreMetric()
+    clip_scorer = CLIPScoreMetric(clip_model_name_or_path=clip_model_name_or_path)
     diffuion_evaluation_pipeline.add_metric(clip_scorer)
 
     # Add CLIP IQA Metric
-    clip_iqa_scorer = CLIPImageQualityScoreMetric()
+    clip_iqa_scorer = CLIPImageQualityScoreMetric(
+        clip_model_name_or_path=clip_iqa_model_name_or_path
+    )
     diffuion_evaluation_pipeline.add_metric(clip_iqa_scorer)
 
     diffuion_evaluation_pipeline(
-        dataset="parti-prompts:v1",
-        init_params=dict(project="t2i_eval", entity="geekyrakshit"),
+        dataset=dataset,
+        init_params=dict(project=project),
+        job_name="test",
     )
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
