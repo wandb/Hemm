880b7d686dc1dc66770b4203d5968800e6b9d0d8
soumik12345 <19soumik.rakshit96@gmail.com>
2024-06-17 11:28:23 +0000

    update: SpatialRelationshipMetric

--- hemm/metrics/spatial_relationship/spatial_relationship.py
+++ hemm/metrics/spatial_relationship/spatial_relationship.py
@@ -1,16 +1,45 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Optional, Union, TYPE_CHECKING
 
 import weave
 
-from .judges import DETRSpatialRelationShipJudge
-from .judges.commons import BoundingBox
+if TYPE_CHECKING:
+    from .judges import DETRSpatialRelationShipJudge
+    from .judges.commons import BoundingBox
+
+
+def get_iou(entity_1: BoundingBox, entity_2: BoundingBox) -> float:
+    x_overlap = max(
+        0,
+        min(entity_1.box_coordinates_max.x, entity_2.box_coordinates_max.x)
+        - max(entity_1.box_coordinates_min.x, entity_2.box_coordinates_min.x),
+    )
+    y_overlap = max(
+        0,
+        min(entity_1.box_coordinates_max.y, entity_2.box_coordinates_max.y)
+        - max(entity_1.box_coordinates_min.y, entity_2.box_coordinates_min.y),
+    )
+    intersection = x_overlap * y_overlap
+    box_1_area = (entity_1.box_coordinates_max.x - entity_1.box_coordinates_min.x) * (
+        entity_1.box_coordinates_max.y - entity_1.box_coordinates_min.y
+    )
+    box_1_area = (entity_2.box_coordinates_max.x - entity_2.box_coordinates_min.x) * (
+        entity_2.box_coordinates_max.y - entity_2.box_coordinates_min.y
+    )
+    union = box_1_area + box_1_area - intersection
+    return intersection / union
 
 
 class SpatialRelationshipMetric:
     def __init__(
-        self, judge: Union[weave.Model, DETRSpatialRelationShipJudge], name: str
+        self,
+        judge: Union[weave.Model, DETRSpatialRelationShipJudge],
+        iou_threshold: float,
+        distance_threshold: float,
+        name: Optional[str] = "spatial_relationship_score",
     ) -> None:
         self.judge = judge
+        self.iou_threshold = iou_threshold
+        self.distance_threshold = distance_threshold
         self.name = name
         self.scores = []
         self.config = judge.model_dump()
@@ -26,9 +55,84 @@ class SpatialRelationshipMetric:
             "entity_2_present": False,
         }
         entities = [entity["name"] for entity in response["entities"]]
+        entity_boxes: List[BoundingBox] = [None, None]
         for box in boxes:
             if box.label == entities[0]:
-                judgement["enity_1_present"] = True
+                judgement["entity_1_present"] = True
+                entity_boxes[0] = box
             elif box.label == entities[1]:
                 judgement["entity_2_present"] = True
+                entity_boxes[1] = box
+
+        center_distance_x = abs(
+            entity_boxes[0].box_coordinates_center.x
+            - entity_boxes[1].box_coordinates_center.x
+        )
+        center_distance_y = abs(
+            entity_boxes[0].box_coordinates_center.y
+            - entity_boxes[1].box_coordinates_center.y
+        )
+        iou = get_iou(entity_boxes[0], entity_boxes[1])
+        score = 0.0
+        if response["relation"] in ["near", "next to", "on side of", "side of"]:
+            if (
+                abs(center_distance_x) < self.distance_threshold
+                or abs(center_distance_y) < self.distance_threshold
+            ):
+                score = 1.0
+            else:
+                score = self.distance_threshold / max(
+                    abs(center_distance_x), abs(center_distance_y)
+                )
+        elif response["relation"] == "on the right of":
+            if center_distance_x < 0:
+                if (
+                    abs(center_distance_x) > abs(center_distance_y)
+                    and iou < self.iou_threshold
+                ):
+                    score = 1.0
+                elif (
+                    abs(center_distance_x) > abs(center_distance_y)
+                    and iou >= self.iou_threshold
+                ):
+                    score = self.iou_threshold / iou
+        elif response["relation"] == "on the left of":
+            if center_distance_x > 0:
+                if (
+                    abs(center_distance_x) > abs(center_distance_y)
+                    and iou < self.iou_threshold
+                ):
+                    score = 1.0
+                elif (
+                    abs(center_distance_x) > abs(center_distance_y)
+                    and iou >= self.iou_threshold
+                ):
+                    score = self.iou_threshold / iou
+            else:
+                score = 0.0
+        elif response["relation"] == "on the bottom of":
+            if center_distance_y < 0:
+                if (
+                    abs(center_distance_y) > abs(center_distance_x)
+                    and iou < self.iou_threshold
+                ):
+                    score = 1
+                elif (
+                    abs(center_distance_y) > abs(center_distance_x)
+                    and iou >= self.iou_threshold
+                ):
+                    score = self.iou_threshold / iou
+        elif response["relation"] == "on the top of":
+            if center_distance_y > 0:
+                if (
+                    abs(center_distance_y) > abs(center_distance_x)
+                    and iou < self.iou_threshold
+                ):
+                    score = 1
+                elif (
+                    abs(center_distance_y) > abs(center_distance_x)
+                    and iou >= self.iou_threshold
+                ):
+                    score = self.iou_threshold / iou
+        judgement["score"] = score
         return judgement
